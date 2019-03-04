c87ba9edccc828fcbaff07be01cb7ad1bccc5201
Joshua Lobo <joshua.jude.lobo@gmail.com>
2019-03-04 12:54:47 +0000

    finalizing scripts before move to scipion fork

--- zo_gautomatch.py
+++ zo_gautomatch.py
@@ -8,10 +8,16 @@ from subprocess import Popen
 import sys
 import os
 
+TIMEOUT = 60
 
 def run_zocalo_gautomatch():
     script = "/home/jtq89441/workspace/zocalo_test/motioncor2_launcher/testing/share_local/zoc_gautomatch_submit.py"
     args = sys.argv[1:]
+
+    automatch_file = str(args[0]).replace('.mrc','_automatch.star')
+
+
+
 
     # TODO: add the part of exit where script exits after creating png of whatever 
 
@@ -26,14 +32,27 @@ def run_zocalo_gautomatch():
 
     p1 = Popen(cmd + " ".join(args), shell=True)
     p1.wait()
-    import time
-    time.sleep(1)
-    # submission to zocalo queue
 
+
+    # sleep for a ridiculous amount of time
     out = p1.communicate()[0]
     print(out)
 
+    import time
     
+    found = False
+    start_time = time.time()
+    while not found:
+    	if os.path.exists(automatch_file):
+    		found = True
+    		break
+    	else:
+    		print("File not found, waiting...")
+    	time.sleep(10)
+
+    	if (time.time() - start_time) > TIMEOUT:
+    		sys.exit(1)
+
     return out
 
    
